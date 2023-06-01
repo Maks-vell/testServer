52ff1bafc97df63801fd83c0af44f642c08c9e32
Maksim Vyalmisov <maks.vell252@gmail.com>
2023-06-02 01:47:08 +0300

    fix timer

--- src/main/java/com/psu/testserver/javafx/controller/SharingController.java
+++ src/main/java/com/psu/testserver/javafx/controller/SharingController.java
@@ -5,12 +5,15 @@ import com.google.gson.GsonBuilder;
 import com.psu.testserver.javafx.client.Client;
 import com.psu.testserver.model.StudentListModel;
 import com.psu.testserver.model.StudentModel;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.control.ListView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import lombok.Setter;
 
 import java.io.IOException;
@@ -51,10 +54,33 @@ public class SharingController implements Controller {
         this.testNameLabel.setText(this.sharingTestName);
 
         this.studentModelList = new ArrayList<>();
-        this.timerGetStudents = new Timer();
-        this.timerGetStudents.schedule(new StudentListUpdater(this.client, this), 0, TIME_BETWEEN_GET_STUDENTS);
+
+        startGetStudents();
 
         this.closeSharingButton.setOnAction(this::closeSharing);
+    }
+
+    private void startGetStudents() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(2), this::getStudents));
+        timeline.setCycleCount(1000);
+        timeline.play();
+    }
+
+    private void getStudents(ActionEvent actionEvent) {
+        String studentListModelJSON = null;
+
+        try {
+            studentListModelJSON = client.requestWithResponse("GET/testService/getStudents");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        StudentListModel studentListModel = gson.fromJson(studentListModelJSON, StudentListModel.class);
+        this.studentModelList = studentListModel.studentModels;
+        studentListViewUpdate();
     }
 
     public void studentListViewUpdate() {
@@ -87,35 +113,5 @@ public class SharingController implements Controller {
 
         Stage stage = (Stage) this.closeSharingButton.getScene().getWindow();
         stage.close();
-    }
-}
-
-class StudentListUpdater extends TimerTask {
-    private Client client;
-
-    private SharingController sharingController;
-
-    public StudentListUpdater(Client client, SharingController sharingController) {
-        this.client = client;
-        this.sharingController = sharingController;
-    }
-
-    public void run() {
-        try {
-            tryGetStudentModelList();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        this.sharingController.studentListViewUpdate();
-    }
-
-    private void tryGetStudentModelList() throws IOException {
-        String studentListModelJSON = client.requestWithResponse("GET/testService/getStudents");
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-
-        StudentListModel studentListModel = gson.fromJson(studentListModelJSON, StudentListModel.class);
-        sharingController.studentModelList = studentListModel.studentModels;
     }
 }
